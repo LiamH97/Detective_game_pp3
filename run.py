1f0c1769316f88eb8692c66547acecdd980ce6b5
LiamH97 <ldharrington5@gmail.com>
2022-01-18 12:20:44 +0000

    added functions to start game (temporary, juust prints that the function is called) Added a function to ask the user if they wish to play. Exits game is no is selected and calls the start_game function if yes is chosen

--- run.py
+++ run.py
@@ -13,14 +13,32 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('detective_game')
 
 def get_username():
+    """ Asks the user for their name and stores it as a variable {name} """
     print("What is your name?")
     name = input()
     return name
 print("Hello " + get_username() +"!")
 
 def read_storyline():
+    """ A function to read from the storyline text file and give the user an idea of how the game works"""
     f = open('storyline.txt', 'r')
     storyline = f.read()
     f.close()
     print(storyline)
-print (read_storyline())
+print(read_storyline())
+
+def start_game():
+    """Temporary function for starting the game to test it is called correctly"""
+    print('start game function called')
+def ask_to_begin():
+    """Asks the user if they would like to begin the game"""
+    print('Would you like to begin? \n 1 = Yes, 2 = No')
+    begin = input()
+    if begin == '1':
+        start_game()
+    elif begin == '2':
+        exit()
+    else: 
+        print('Invalid entry')
+        ask_to_begin()
+ask_to_begin()
