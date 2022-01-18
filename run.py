a48e0e29a1f4c1028f154047967cb4fc02c46d49
LiamH97 <ldharrington5@gmail.com>
2022-01-18 11:54:11 +0000

    Testing sheets API and reading values from sheet

--- run.py
+++ run.py
@@ -11,3 +11,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('detective_game')
+
+status = SHEET.worksheet('Status')
+data = status.get_all_values()
+print(data)
